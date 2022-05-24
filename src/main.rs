34535049cf231641fa4118271382bb6ce7d18834
gus <gustavo_9_rj@hotmail.com>
2022-05-24 21:41:46 +0200

    API responde JSON por los endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+
+#[derive(Serialize)]
+struct SalesAnalyticsResponse {
+    successful: bool,
+    message: &'static str,
+    analytics: Option<Vec<&'static str>>,
+}
+
+#[get("/")]
+fn api() -> &'static str {
+    "API Description"
+}
+
+#[get("/sales")]
+fn sales() -> Result<Json<SalesAnalyticsResponse>, String> {
+    let res = SalesAnalyticsResponse {
+        successful: true,
+        message: "Hola mundo!!",
+        analytics: None,
+    };
+    let ventas = Json(res);
+
+    return Ok(ventas);
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api", routes![api, sales])
 }
