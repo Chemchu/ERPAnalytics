ed67a897fc481b16a667d01b466c8bd30a2f2b07
gus <gustavo_9_rj@hotmail.com>
2022-05-27 04:33:23 +0200

    Deserealiza JSON

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #![allow(non_snake_case)]
 #[macro_use]
 extern crate rocket;
+extern crate juniper;
 
 use rocket::response::content;
 use rocket::serde::json::Json;
 use rocket::serde::Serialize;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GraphQLReqBody {
+    query: String,
+    variables: Option<String>,
+}
 
 #[derive(Serialize)]
 struct SalesAnalyticsResponse {
@@ -35,9 +43,22 @@ fn sales() -> Result<Json<SalesAnalyticsResponse>, String> {
     return Ok(Json(res));
 }
 
+// Se encarga de escuchar las peticiones en GraphQL para reenviar al microservicio indicado
+#[post("/", format = "json", data = "<body>")]
+fn graphql(body: Json<GraphQLReqBody>) -> Result<Json<String>, String> {
+    let res = String::from("GraphQL reforward");
+    let v = body.variables.clone().unwrap();
+
+    println!("{}", body.query);
+    println!("{}", v);
+
+    return Ok(Json(res));
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/api", routes![api, sales])
+        .mount("/graphql", routes![graphql])
 }
