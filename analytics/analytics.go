e492f1dd518956b6225092d6698a9ff3f29e6550
gus <gustavo_9_rj@hotmail.com>
2022-06-28 14:47:48 +0200

    Ya devuelve el summary pero en formato string en lugar de objeto

---
+++ analytics/analytics.go
@@ -0,0 +1,137 @@
+package analitycs
+
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+
+	"github.com/Chemchu/ERPAnalytics/types"
+)
+
+func GetSalesSummaryByDay(ventas []types.Venta) types.APIResponse {
+	// data := fmt.Sprintf("%+v", Summarize(&ventas))
+	// data := Summarize(&ventas)
+	msg := "Petición realizada correctamente"
+	successful := true
+	data, err := json.Marshal(Summarize(&ventas))
+	if err != nil {
+		panic("No se ha podido convertir el Summary a JSON")
+	}
+
+	return types.APIResponse{
+		Message:    msg,
+		Successful: successful,
+		Data:       string(data),
+	}
+}
+
+func MapToArray(d map[string]types.VentasPorHora) []types.VentasPorHora {
+	m := make([]types.VentasPorHora, 0, len(d))
+	for _, val := range d {
+		m = append(m, val)
+	}
+
+	return m
+}
+
+func GetMostFrequentValue(array []string) string {
+	// Collect words and their count in a histogram.
+	histo := make(map[string]int)
+	for _, str := range array {
+		histo[str]++
+	}
+
+	// Scan the histogram to find the word with the highest count.
+	frecuencia := 0
+	palabraMasFrecuente := ""
+	for key, value := range histo {
+		if value > frecuencia {
+			frecuencia = value
+			palabraMasFrecuente = key
+		}
+	}
+	return palabraMasFrecuente
+}
+
+func Summarize(ventas *[]types.Venta) types.Summary {
+	beneficioTotal := 0.0
+	dineroDescontadoTotal := 0.0
+	total := 0.0
+	ivaPagado := 0.0
+	prodVendidosTotal := 0
+	ventasPorHoraMap := make(map[string]types.VentasPorHora)
+	ventasPorHora := MapToArray(ventasPorHoraMap)
+	numVentas := len(*ventas)
+	mediaVentas := total / float64(numVentas)
+	mediaCantidadVendida := 0.0
+
+	if len(*ventas) <= 0 {
+		return types.Summary{
+			VentasPorHora:             ventasPorHora,
+			Beneficio:                 beneficioTotal,
+			TotalVentas:               total,
+			NumVentas:                 numVentas,
+			DineroDescontado:          dineroDescontadoTotal,
+			CantidadProductosVendidos: prodVendidosTotal,
+			MediaVentas:               mediaVentas,
+			MediaCantidadVenida:       mediaCantidadVendida,
+			IVAPagado:                 ivaPagado,
+		}
+	}
+
+	for _, venta := range *ventas {
+		// Inicializa los valores
+		beneficioHora := 0.0
+		dineroDescontadoHora := 0.0
+		totalHora := 0.0
+		prodVendidosHora := 0
+		hora := strconv.FormatInt(int64(time.UnixMilli(venta.CreatedAt).Hour()), 10)
+
+		// Comprueba si ya hay ventas añadidas para una deterrminada hora
+		if ventaEnMap, containsValue := ventasPorHoraMap[hora]; containsValue {
+			beneficioHora = ventaEnMap.BeneficioHora
+			totalHora = ventaEnMap.TotalVentaHora
+			prodVendidosHora = ventaEnMap.ProductosVendidosHora
+			dineroDescontadoHora = ventaEnMap.DineroDescontadoHora
+		}
+
+		for _, producto := range venta.Productos {
+			beneficioTotal += (producto.PrecioFinal - producto.PrecioCompra) * float64(producto.CantidadVendida)
+			beneficioHora += (producto.PrecioFinal - producto.PrecioCompra) * float64(producto.CantidadVendida)
+			total += producto.PrecioFinal * float64(producto.CantidadVendida)
+			totalHora += producto.PrecioFinal * float64(producto.CantidadVendida)
+			prodVendidosTotal += producto.CantidadVendida
+			prodVendidosHora += producto.CantidadVendida
+			dineroDescontadoHora += (producto.PrecioVenta - producto.PrecioFinal) * float64(producto.CantidadVendida)
+			ivaPagado += producto.PrecioCompra * (producto.Iva / 100)
+		}
+
+		venta := types.VentasPorHora{
+			Hora:                  hora,
+			BeneficioHora:         beneficioHora,
+			TotalVentaHora:        totalHora,
+			ProductosVendidosHora: prodVendidosHora,
+			DineroDescontadoHora:  dineroDescontadoHora,
+		}
+		ventasPorHoraMap[hora] = venta
+	}
+
+	ventasPorHora = MapToArray(ventasPorHoraMap)
+	numVentas = len(*ventas)
+	mediaVentas = total / float64(numVentas)
+	if numVentas > 0 {
+		mediaCantidadVendida = float64(prodVendidosTotal / numVentas)
+	}
+
+	return types.Summary{
+		VentasPorHora:             ventasPorHora,
+		Beneficio:                 beneficioTotal,
+		TotalVentas:               total,
+		NumVentas:                 numVentas,
+		DineroDescontado:          dineroDescontadoTotal,
+		CantidadProductosVendidos: prodVendidosTotal,
+		MediaVentas:               mediaVentas,
+		MediaCantidadVenida:       mediaCantidadVendida,
+		IVAPagado:                 ivaPagado,
+	}
+}
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Chemchu/ERPAnalytics/analitycs"
+	analitycs "github.com/Chemchu/ERPAnalytics/analytics"
 	"github.com/Chemchu/ERPAnalytics/types"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -15,17 +15,33 @@ func getAPI(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Bienvenido al API de Análisis de datos de ERPSolution", "successful": true})
 }
 
+// type Prueba struct {
+// 	Msg string `json:"msg"`
+// 	//Data Prueba2 `json:"data,omitempty"`
+// }
+
+// type Prueba2 struct {
+// 	Succ bool `json:"msg"`
+// }
+
 func postSummarizeSales(c *gin.Context) {
-	// body, _ := ioutil.ReadAll(c.Request.Body)
-	// println(string(body))
-	var ventasPointer []types.Venta
-	if err := c.ShouldBindJSON(&ventasPointer); err != nil {
+	var ventas []types.Venta
+	if err := c.ShouldBindJSON(&ventas); err != nil {
 		fmt.Printf("Error: %+v\n", err)
 		c.JSON(http.StatusOK, gin.H{"message": err, "successful": false})
 		return
 	}
-	// fmt.Println(*ventasPointer[0].ID)
-	summaryResponse := analitycs.GetSalesSummaryByDay(&ventasPointer)
+
+	// p2 := Prueba2{
+	// 	Succ: true,
+	// }
+	// p1 := Prueba{
+	// 	Msg: "prueba1",
+	// 	//Data: p2,
+	// }
+	// pRes, _ := json.Marshal(&p1)
+
+	summaryResponse := analitycs.GetSalesSummaryByDay(ventas)
 	c.JSON(http.StatusOK, summaryResponse)
 }
 
